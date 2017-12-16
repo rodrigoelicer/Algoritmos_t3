f00933572f6cc0e61866ad746bbae3572fa158c3
Rodrigo Elicer Krause <rodrigo.elicer1@gmail.com>
2017-12-16 04:13:42 -0300

    recursión funcionando

--- main.cpp
+++ main.cpp
@@ -4,44 +4,64 @@ using namespace std;
 
 int max (int x, int y) { return (x > y)? x : y; }
 
-int lps(char *str){
-  int n = strlen(str);
-  int i, j, cl;
-  int L[n][n];  // Create a table to store results of subproblems
+void printMatriz(vector <vector<int>> &matriz){
+	size_t n = matriz.size(), i;
 
+	for( i = 0; i<n ; i++){
+		size_t m = matriz[i].size(), j;
 
-  // Strings of length 1 are palindrome of lentgh 1
-  for (i = 0; i < n; i++)
-	 L[i][i] = 1;
-
-   // Build the table. Note that the lower diagonal values of table are
-   // useless and not filled in the process. The values are filled in a
-   // manner similar to Matrix Chain Multiplication DP solution (See
-   // http://www.geeksforgeeks.org/archives/15553). cl is length of
-   // substring
-   for (cl=2; cl<=n; cl++)
-   {
-	   for (i=0; i<n-cl+1; i++)
-	   {
-		   j = i+cl-1;
-		   if (str[i] == str[j] && cl == 2)
-			  L[i][j] = 2;
-		   else if (str[i] == str[j])
-			  L[i][j] = L[i+1][j-1] + 2;
-		   else
-			  L[i][j] = max(L[i][j-1], L[i+1][j]);
-	   }
-   }
-
-   return L[0][n-1];
+		cout << "[";
+		for( j = 0; j<m; j++){
+			cout << matriz[i][j] << ", ";
+		}
+		cout << "]" << endl;
+	}
 }
 
-/* Driver program to test above functions */
-int main()
-{
-   char seq[] = "GEEKS FOR GEEKS";
-   int n = strlen(seq);
-   printf ("The lnegth of the LPS is %d", lps(seq));
-   getchar();
-   return 0;
+void lps(vector <vector<int>> &matriz, vector <string> &arr, int n){
+	int i;
+
+	string str = arr.back();
+
+	//crea nueva columna
+	vector <int> nuevo;
+	matriz.push_back(nuevo);
+
+	matriz[n].push_back(1);
+	cout << endl;
+	//cout << "n: " << n << endl;
+	for( i = 0; i < n; i++ ){
+		//cout << "str: " << str << endl;
+		//cout << "arr[" << n-i-1 << "]: " << arr[n-i-1] << endl;
+		if ( str == arr[n-i-1] && n == 1 ){
+			//cout << "entre1" << endl;
+			matriz[n].push_back(2);
+		}
+		else if ( str == arr[n-i-1] ){
+			//cout << "entre2" << endl;
+			matriz[n].push_back( 2 + matriz[n-1][i-1] );
+		}
+		else{
+			//cout << "entre3" << endl;
+			matriz[n].push_back( max(matriz[n][i], matriz[n-1][i]) );
+		}
+	}
+	printMatriz(matriz);
+
+   return;
+}
+
+int main(){
+	vector <vector<int>> matriz;
+	vector <string> arr;
+	string str;
+	int n = -1;
+
+	while( cin >> str ){
+		arr.push_back(str);
+		n++;
+		lps(matriz, arr, n);
+	}
+
+	return 0;
 }
